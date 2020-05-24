868b4fe18c0cc0084fa2011bfbb79d45dcaa40bb
Guillermo Sanchez <guillermo.sanchez@utec.edu.pe>
2020-05-24 00:14:15 -0500

    Ahora si se ve el message

---
+++ web/static/js/libs/messages.js
@@ -0,0 +1,50 @@
+$(function(){
+    var url = "/messages";
+     $("#grid").dxDataGrid({
+         dataSource: DevExpress.data.AspNet.createStore({
+             key: "id",
+             insertUrl: url,
+             updateUrl: url,
+             deleteUrl: url,
+             loadUrl: url,
+             onBeforeSend: function(method, ajaxOptions) {
+                 ajaxOptions.xhrFields = { withCredentials: true };
+             }
+         }),
+
+         editing: {
+             allowUpdating: true,
+             allowDeleting: true,
+             allowAdding: true
+         },
+
+         remoteOperations: {
+             sorting: true,
+             paging: true
+         },
+
+         paging: {
+             pageSize: 12
+         },
+
+         pager: {
+             showPageSizeSelector: true,
+             allowedPageSizes: [8, 12, 20]
+         },
+
+         columns: [{
+             dataField: "id",
+             dataType: "number",
+             allowEditing: true
+         }, {
+             dataField: "content"
+         }, {
+             dataField: "sent_on",
+             allowEditing: true
+         }, {
+             dataField: "user_from_id"
+         }, {
+             dataField: "user_to_id"}]
+
+     }).dxDataGrid("instance");
+    });
